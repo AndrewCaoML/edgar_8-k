dcbb5e18a89c6d79b054392c85064bd57f47447f
Andrew <andrew.cao.mn@gmail.com>
2020-10-22 15:53:48 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,3 +12,10 @@ sheet = client.open('Database').worksheet("BIOTECHs")
 all_comps = sheet.col_values(1)
 all_comps.remove('Active Biotechs')
 
+imported_companies = client.open('Trial Database').worksheet("Historical")
+imports = imported_companies.col_values(1)
+
+imported_trials = client.open('Trial Database').worksheet("Drugs/Conditions")
+trial_comps = imported_trials.col_values(1)
+trial_conds = imported_trials.col_values(3)
+trial_drugs = imported_trials.col_values(4)
